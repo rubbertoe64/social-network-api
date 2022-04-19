8d77e00dfdeb9876d7b51c900d2b614aa4e4f14d
Roberto Gonzalez <rgonzalez7359@gmail.com>
2022-04-19 01:31:24 -0700

    added thoughts and friends arrays from object IDs and virtual

--- models/User.js
+++ models/User.js
@@ -12,30 +12,38 @@ const userSchema = new Schema({
         required: true,
         unique: true,
         trim: true},
+
     email: {
         type: String, 
         required: true,
         unique: true,   
         validate: [validateEmail, 'Please fill a valid email address'],
-        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']}
-}
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']},
+
+    thoughts:[{ 
+        type: Schema.Types.ObjectId, 
+        ref: 'Thoughts' }],
+    
+    friends:[{ 
+        type: Schema.Types.ObjectId, 
+        ref: 'Thoughts' }],
+
+    toJSON: {
+        virtuals: true,
+        getters: true
+    },
+    id: false
+        
+
   
-);
+    });
 
-// Create a virtual property `commentCount` that gets the amount of comments per user
 userSchema
-  .virtual('fullName')
-  // Getter
+  .virtual('friendCount')
   .get(function () {
-    return `${this.first} ${this.last}`;
+      return this.friends.length
   })
-  // Setter to set the first and last name
-  .set(function (v) {
-    const first = v.split(' ')[0];
-    const last = v.split(' ')[1];
-    this.set({ first, last });
-  });
-
+ 
 // Initialize our User model
 const User = model('user', userSchema);
 
