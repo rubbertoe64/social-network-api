0129a3a418627bdae7fef1fa06c96902dba8dcd7
Roberto Gonzalez <rgonzalez7359@gmail.com>
2022-04-19 01:06:51 -0700

    added validationg and match for email

--- models/User.js
+++ models/User.js
@@ -1,9 +1,23 @@
 const { Schema, model } = require('mongoose');
 
+var validateEmail = function(email) {
+    var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return re.test(email)
+};
+
 // Schema to create User model
 const userSchema = new Schema({
-    username: {type: String, required: true,unique: true,trim: true
-    }
+    username: {
+        type: String, 
+        required: true,
+        unique: true,
+        trim: true},
+    email: {
+        type: String, 
+        required: true,
+        unique: true,   
+        validate: [validateEmail, 'Please fill a valid email address'],
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']}
 }
   
 );
